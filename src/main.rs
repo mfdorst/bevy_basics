fba0cc563c71a3fa03b454e3397a337b494ea146
Michael Dorst <m@mdorst.net>
2021-12-21 20:14:42 -0800

    Rustacean can scuttle!

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,18 @@
 use bevy::prelude::*;
 
 const PLAYER_A_SPRITE: &str = "player_a.png";
+const TIME_STEP: f32 = 1.0 / 60.0;
+
+// ### Resources ###
+
+struct Materials {
+    player_material: Handle<ColorMaterial>,
+}
+
+// ### Components ###
+
+struct Player;
+struct PlayerSpeed(f32);
 
 fn main() {
     App::build()
@@ -13,8 +25,15 @@ fn main() {
         })
         .add_plugins(DefaultPlugins)
         .add_startup_system(setup.system())
+        .add_startup_stage(
+            "game_setup_actors",
+            SystemStage::single(player_spawn.system()),
+        )
+        .add_system(player_movement.system())
         .run();
 }
+
+// ## Systems ##
 
 fn setup(
     mut commands: Commands,
@@ -22,19 +41,47 @@ fn setup(
     mut materials: ResMut<Assets<ColorMaterial>>,
     mut windows: ResMut<Windows>,
 ) {
+    // TODO: Error handling
+    let window = windows.get_primary_mut().unwrap();
     commands.spawn_bundle(OrthographicCameraBundle::new_2d());
 
-    let window = windows.get_primary_mut().unwrap();
-    window.set_position(IVec2::new(750, 200));
-
-    let bottom = -window.height() / 2.0;
-    commands.spawn_bundle(SpriteBundle {
-        material: materials.add(asset_server.load(PLAYER_A_SPRITE).into()),
-        transform: Transform {
-            translation: Vec3::new(0.0, bottom + 25.0, 10.0),
-            scale: Vec3::new(0.5, 0.5, 1.0),
-            ..Default::default()
-        },
-        ..Default::default()
+    commands.insert_resource(Materials {
+        player_material: materials.add(asset_server.load(PLAYER_A_SPRITE).into()),
     });
+
+    window.set_position(IVec2::new(750, 200));
+}
+
+fn player_spawn(mut commands: Commands, materials: Res<Materials>) {
+    let bottom = -300.0;
+    let material = materials.player_material.clone();
+    let transform = Transform {
+        translation: Vec3::new(0.0, bottom + 25.0, 10.0),
+        scale: Vec3::new(0.5, 0.5, 1.0),
+        ..Default::default()
+    };
+    commands
+        .spawn_bundle(SpriteBundle {
+            material,
+            transform,
+            ..Default::default()
+        })
+        .insert(Player)
+        .insert(PlayerSpeed(500.0));
+}
+
+fn player_movement(
+    keyboard_input: Res<Input<KeyCode>>,
+    mut query: Query<(&PlayerSpeed, &mut Transform, With<Player>)>,
+) {
+    if let Ok((PlayerSpeed(speed), mut transform, _)) = query.single_mut() {
+        let dir = if keyboard_input.pressed(KeyCode::Left) {
+            -1.0
+        } else if keyboard_input.pressed(KeyCode::Right) {
+            1.0
+        } else {
+            0.0
+        };
+        transform.translation.x += dir * speed * TIME_STEP;
+    }
 }
